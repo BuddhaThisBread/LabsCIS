db598265fc27e395541ce48356ac48d1cd5e7dc4
BuddhaThisBread <40405216+BuddhaThisBread@users.noreply.github.com>
2021-03-17 16:08:12 -0500

    finished, just need to debug one thing...

--- Proj5.java
+++ Proj5.java
@@ -10,13 +10,15 @@ public class Proj5 {
 	public static void main(String[] args) throws IOException{
 		Scanner scnr = new Scanner (System.in);
 		int [][] landArray = readBoard("life1.txt");
-		
-///////////////TEST__OUTPUT///////////////////
-System.out.print(boardDisplay(landArray));
-
-System.out.print(lookingForLife(landArray, Integer.parseInt(args[0]), Integer.parseInt(args[1])));
-///////////////TEST__OUTPUT///////////////////
-	
+		System.out.println(boardDisplay(landArray));
+		System.out.println(neighbors(landArray, 7, 2));
+		while (true) {
+			System.out.println(boardDisplay((update(landArray)))+ "\n");
+			try {
+				Thread.sleep(500);
+			}
+				catch (InterruptedException e) {}
+		}	
 	}
 	
 	/*
@@ -152,7 +154,7 @@ System.out.print(lookingForLife(landArray, Integer.parseInt(args[0]), Integer.pa
 		}
 		
 		/*
-		 	lookingForLife searches the 2D grid (the array containing the grid) keeps 
+		 	neighbors searches the 2D grid (the array containing the grid) keeps 
 		 		a running total of living cells surrounding the current cell being looked at
 		 	@int [][] landArray - obtains the 2D grid containing the 1's and 0's
 		 	@iRef - obtains the current cells's location with respect to its row
@@ -160,129 +162,104 @@ System.out.print(lookingForLife(landArray, Integer.parseInt(args[0]), Integer.pa
 		 	@return int lifeForm - returns amount of neighboring cells that are alive
 		*/
 		
-		public static int lookingForLife(int[][] landArray, int iRef, int jRef) {
-			
-			int resetJ = jRef; // reset jRef
-			int colCount = 0; // track col were checking
+		public static int neighbors(int[][] landArray, int iRef, int jRef) {
 			int totalLifeForm = 0; // what we want to return
 			
-			if (iRef - 1 < 0 && jRef - 1 < 0) { // upper left corner				
+			if (iRef - 1 < 0 && jRef - 1 < 0) { // upper left corner
+
 				totalLifeForm += scanCurrentRow(landArray, iRef, jRef + 1, 2, 1); // scan first row, start at 1st col
 				totalLifeForm += scanFinalRow(landArray, iRef, jRef + 1, 1, 0);
-				
 				return totalLifeForm;
 			} // end of upper left corner 
 			
-			else if (jRef - 1 < 0) { // if on left edge of board
-				totalLifeForm += scanFirstRow(landArray, iRef, jRef + 1, 1, 1);
-				totalLifeForm += scanCurrentRow(landArray, iRef, jRef + 1, 1, 1);
-				totalLifeForm += scanFinalRow(landArray, iRef, jRef + 1, 1, 1);
-				
-				return totalLifeForm;
-			} // end of left edge border case
-			
 			else if(jRef - 1 < 0 && iRef + 1 == landArray.length) { // bottom left corner
+
 				totalLifeForm += scanFirstRow(landArray, iRef, (jRef + 1), 1, 1);
 				totalLifeForm += scanCurrentRow(landArray, iRef, (jRef + 1), 2, 1);
 				return totalLifeForm;
-			}
+			} // end of bot left corner
 			
-			/*else if (iRef - 1 < 0) { // top border case
-				do { // scan current row
-					if (landArray[iRef][jRef - 1] == 1) {
-						lifeForm++;
-						jRef += 2;
-						colCount++;
-					} else if (landArray[iRef][jRef - 1] == 0) {
-						jRef += 2;
-						colCount++;
-					}
-				} while(colCount < 2);
-				
-				jRef = resetJ;
-				colCount = 0;
-				
-				do { // scan last row
-					if (landArray[iRef + 1][jRef - 1] == 1) {
-						lifeForm++;
-						colCount++;
-						jRef++;
-					} else if (landArray[iRef + 1][jRef - 1] == 0) {
-						colCount++;
-						jRef++;
-					}
-				} while(colCount < 3);
-				return lifeForm;
-			} // end of top border case
+			else if (jRef - 1 < 0) { // if on left edge of board
+
+				totalLifeForm += scanFirstRow(landArray, iRef, jRef + 1, 1, 1);
+				totalLifeForm += scanCurrentRow(landArray, iRef, jRef + 1, 1, 1);
+				totalLifeForm += scanFinalRow(landArray, iRef, jRef + 1, 1, 1);
+				return totalLifeForm;
+			} // end of left edge border case
 			
-			else if(iRef + 1 == landArray.length) { // bottom edge case
-				do { // scan previous row
-					if (landArray[iRef - 1][jRef - 1] == 1) {
-						lifeForm++;
-						colCount++;
-						jRef++; // move to next neighbor
-					} else if (landArray[iRef - 1][jRef - 1] == 0) {
-						colCount++;
-						jRef++;
-					}
-				} while(colCount < 3);
-				
-				jRef = resetJ; // reset everything
-				colCount = 0;
-				
-				do { // scan current row
-					if (landArray[iRef][jRef - 1] == 1) {
-						lifeForm++;
-						jRef += 2;
-						colCount++;
-					} else if (landArray[iRef][jRef - 1] == 0) {
-						jRef += 2;
-						colCount++;
-					}
-				} while(colCount < 2);
-				return lifeForm;
-			} // end of bottom edge case
+			else if ((iRef - 1 < 0) && (jRef != landArray[0].length - 1)){ // top edge 
+
+				totalLifeForm += scanCurrentRow(landArray, iRef, jRef, 2, 2);
+				totalLifeForm += scanFinalRow(landArray, iRef, jRef, 1, 1);
+				return totalLifeForm;
+			} // end of top edge 
 			
-				do { // scan previous row
-					if (landArray[iRef - 1][jRef - 1] == 1) {
-						lifeForm++;
-						colCount++;
-						jRef++; // move to next neighbor
-					} else if (landArray[iRef - 1][jRef - 1] == 0) {
-						colCount++;
-						jRef++;
+			else if ((jRef == landArray[0].length - 1) && (iRef > 0 && iRef < landArray.length - 1)) { // for right edge 
+
+				totalLifeForm += scanFirstRow(landArray, iRef, jRef, 1, 1);
+				totalLifeForm += scanCurrentRow(landArray, iRef, jRef, 0, 3);
+				totalLifeForm += scanFinalRow(landArray, iRef, jRef, 1, 1);
+				return totalLifeForm;
+			} // end of right edge
+			
+			else if ((iRef == landArray.length - 1) && (jRef == landArray[0].length - 1)) { // bottom right corner
+
+				totalLifeForm += scanFirstRow(landArray, iRef, jRef, 1, 1);
+				totalLifeForm += scanCurrentRow(landArray, iRef, jRef, 0, 2);
+				return totalLifeForm;
+			} // end of bot right corner
+			
+			else if((jRef == landArray[0].length - 1) && (iRef - 1 < 0)) { // top right corner
+
+				totalLifeForm += scanCurrentRow(landArray, iRef, jRef, 0, 2);
+				totalLifeForm += scanFinalRow(landArray, iRef, jRef, 1, 1);
+				return totalLifeForm;
+			} // end of top right
+			
+			else if((jRef > 0 && jRef < landArray[0].length) && (iRef == landArray.length - 1)) { // bot edge
+
+				totalLifeForm += scanFirstRow(landArray, iRef, jRef + 1, 1, 1);
+				totalLifeForm += scanCurrentRow(landArray, iRef, jRef + 2, 1, 2);
+				return totalLifeForm;
+			} // end bot edge
+			
+			else { // everything else
+
+				totalLifeForm += scanFirstRow(landArray, iRef, jRef, 1, 0);
+				totalLifeForm += scanCurrentRow(landArray, iRef, jRef, 2, 1);
+				totalLifeForm += scanFinalRow(landArray, iRef, jRef, 1, 0);
+				return totalLifeForm;
+			} // last of it
+		}
+		
+		/*
+		 	update determines if a cell lives or dies or is brought back to life
+		 	@int [][] landArray - takes 2D grid of game board
+		 	@int iRef - refers to row of landArray
+		 	@int jRef - refers to column of landArray
+		 	@return int [][] landArray - returns updated array
+		*/
+		
+		public static int [][] update(int [][] landArray) {
+			for (int i = 0; i < landArray.length; i++) {
+				for (int j = 0; j < landArray[0].length; j++) {
+					int life = neighbors(landArray, i, j);
+					if(landArray[i][j] == 1) { // if reference cell alive
+						if (life < 2 || life > 3) {
+							landArray[i][j] = 0;
+						} else {
+							continue;
+						}
+					} else if (landArray[i][j] == 0) { // if cell dead
+						if (life == 3) {
+							landArray[i][j] = 1;
+						} else {
+							continue;
+						}
 					}
-				} while(colCount < 3);
-				
-				jRef = resetJ; // reset everything
-				colCount = 0;
-				
-				do { // scan current row
-					if (landArray[iRef][jRef - 1] == 1) {
-						lifeForm++;
-						jRef += 2;
-						colCount++;
-					} else if (landArray[iRef][jRef - 1] == 0) {
-						jRef += 2;
-						colCount++;
-					}
-				} while(colCount < 2);
-				
-				jRef = resetJ;
-				colCount = 0;
-				
-				do { // scan last row
-					if (landArray[iRef + 1][jRef - 1] == 1) {
-						lifeForm++;
-						colCount++;
-						jRef++;
-					} else if (landArray[iRef + 1][jRef - 1] == 0) {
-						colCount++;
-						jRef++;
-					}
-				} while(colCount < 3);*/
-				
-			return 0;
+				} // end of inner loop
+			} // end of outer loop
+			return landArray;
 		}
 
 }
