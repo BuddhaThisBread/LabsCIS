665883869f0c48abeb73532d919426158b783dc5
BuddhaThisBread <40405216+BuddhaThisBread@users.noreply.github.com>
2021-03-17 19:39:58 -0500

    added args part

--- Proj5.java
+++ Proj5.java
@@ -1,4 +1,13 @@
-
+/*
+ 	<Proj5.java>
+ 	<Favian Lininger / C / Friday - 330:530>
+ 	
+ 	<Program reads in a file from user, which contains a grid of 1s and 0s and 
+ 	<converts said grid into integers. Program then proceeds to play game of life.
+ 	<Program scans each element from grid and looks for any living neighbors, based 
+ 	<how many neighbors are living, the reference cell will die or if it was already dead
+ 	<resurect.
+ */
 
 import java.io.File;
 import java.io.IOException;
@@ -9,17 +18,23 @@ public class Proj5 {
 
 	public static void main(String[] args) throws IOException{
 		Scanner scnr = new Scanner (System.in);
-		int [][] landArray = readBoard("life2.txt");
-
-		while (true) {
-			System.out.print(boardDisplay(landArray));
-			try {
-				Thread.sleep(500);
-			}
-				catch (InterruptedException e) {}
-			landArray = update(landArray);
+		
+		if (args.length != 1) { // correct user input
+			System.out.print("Usage: java Proj5 filename");
+			return;
 		}
 		
+		int [][] landArray = readBoard(args[0]);
+		scnr.close();
+		
+		while (true) {
+			System.out.print(boardDisplay(landArray)); // display array
+			try {
+				Thread.sleep(300); // delay iteration .3sec
+			}
+				catch (InterruptedException e) {}
+			landArray = update(landArray); // pass updated array
+		}
 	}
 	
 	/*
@@ -27,6 +42,7 @@ public class Proj5 {
 	 	@String fName - passes name of file from user keyboard 
 	 	@return landArray - returns array created from given rows and columns 
 	*/
+	
 	public static int [][] readBoard(String fName) throws IOException {
 		Scanner getLand = new Scanner (new File(fName));
 		
@@ -113,9 +129,9 @@ public class Proj5 {
 		
 		public static int scanCurrentRow(int[][] landArray, int iRef, int jRef, int incrementAmount, int colStart) {
 			int lifeForm = 0;
-			int colCount = colStart;
-			do { // scan current row
-				if (landArray[iRef][jRef - 1] == 1) {
+			int colCount = colStart; // depending on case, scan only 2 instead of 3
+			do { 
+				if (landArray[iRef][jRef - 1] == 1) { // checks if living
 					lifeForm++;
 					colCount++;
 					jRef += incrementAmount;
@@ -123,12 +139,12 @@ public class Proj5 {
 					colCount++;
 					jRef += incrementAmount;
 				}
-			} while(colCount < 3);
+			} while(colCount < 3); // keep track of columns scanned 
 			return lifeForm;
 		}
 		
 		/*
-		 	scanSecondRow - scans row below reference cell
+		 	scanFinalRow - scans row below reference cell
 		 	@int [][] landArray - obtains the 2D grid containing the 1's and 0's
 	 		@int iRef - obtains the current cells's location with respect to its row
 	 		@int jRef - obtains the current cell's location with respect to its column
@@ -140,7 +156,7 @@ public class Proj5 {
 		public static int scanFinalRow(int[][] landArray, int iRef, int jRef, int incrementAmount, int colStart) {
 			int lifeForm = 0;
 			int colCount = colStart;
-			do { // scan last row
+			do {
 				if (landArray[iRef + 1][jRef - 1] == 1) {
 					lifeForm++;
 					colCount++;
@@ -167,21 +183,21 @@ public class Proj5 {
 			int totalLifeForm = 0; // what we want to return
 			
 			if (iRef - 1 < 0 && jRef - 1 < 0) { // upper left corner
-
+				// start with currentRow as were starting at top of array
 				totalLifeForm += scanCurrentRow(landArray, iRef, jRef + 1, 2, 1); // scan first row, start at 1st col
 				totalLifeForm += scanFinalRow(landArray, iRef, jRef + 1, 1, 0);
 				return totalLifeForm;
 			} // end of upper left corner 
 			
 			else if(jRef - 1 < 0 && iRef + 1 == landArray.length) { // bottom left corner
-
+				// skip finalRow as that's out of bounds
 				totalLifeForm += scanFirstRow(landArray, iRef, (jRef + 1), 1, 1);
 				totalLifeForm += scanCurrentRow(landArray, iRef, (jRef + 1), 2, 1);
 				return totalLifeForm;
 			} // end of bot left corner
 			
 			else if (jRef - 1 < 0) { // if on left edge of board
-
+				// start jRef at 1, so as to skip the first column, as thats out of bounds
 				totalLifeForm += scanFirstRow(landArray, iRef, jRef + 1, 1, 1);
 				totalLifeForm += scanCurrentRow(landArray, iRef, jRef + 1, 1, 1);
 				totalLifeForm += scanFinalRow(landArray, iRef, jRef + 1, 1, 1);
@@ -189,14 +205,14 @@ public class Proj5 {
 			} // end of left edge border case
 			
 			else if ((iRef - 1 < 0) && (jRef != landArray[0].length - 1)){ // top edge 
-
+				// skip FirstRow, as were already at first row, index 0
 				totalLifeForm += scanCurrentRow(landArray, iRef, jRef, 2, 2);
 				totalLifeForm += scanFinalRow(landArray, iRef, jRef, 1, 1);
 				return totalLifeForm;
 			} // end of top edge 
 			
 			else if ((jRef == landArray[0].length - 1) && (iRef > 0 && iRef < landArray.length - 1)) { // for right edge 
-
+				// only check for 2 indexes as thrid is out of bounds, set colCount to 1 to ignore last column
 				totalLifeForm += scanFirstRow(landArray, iRef, jRef, 1, 1);
 				totalLifeForm += scanCurrentRow(landArray, iRef, jRef, 0, 3);
 				totalLifeForm += scanFinalRow(landArray, iRef, jRef, 1, 1);
@@ -204,28 +220,28 @@ public class Proj5 {
 			} // end of right edge
 			
 			else if ((iRef == landArray.length - 1) && (jRef == landArray[0].length - 1)) { // bottom right corner
-
+				// skip last row as its out of bounds
 				totalLifeForm += scanFirstRow(landArray, iRef, jRef, 1, 1);
 				totalLifeForm += scanCurrentRow(landArray, iRef, jRef, 0, 2);
 				return totalLifeForm;
 			} // end of bot right corner
 			
 			else if((jRef == landArray[0].length - 1) && (iRef - 1 < 0)) { // top right corner
-
+				// skip first row as its out of bounds
 				totalLifeForm += scanCurrentRow(landArray, iRef, jRef, 0, 2);
 				totalLifeForm += scanFinalRow(landArray, iRef, jRef, 1, 1);
 				return totalLifeForm;
 			} // end of top right
 			
 			else if((jRef > 0 && jRef < landArray[0].length) && (iRef == landArray.length - 1)) { // bot edge
-
+				// skip final row as its out of bounds
 				totalLifeForm += scanFirstRow(landArray, iRef, jRef + 1, 1, 1);
 				totalLifeForm += scanCurrentRow(landArray, iRef, jRef + 2, 1, 2);
 				return totalLifeForm;
 			} // end bot edge
 			
 			else { // everything else
-
+				// takes care of everything else :)
 				totalLifeForm += scanFirstRow(landArray, iRef, jRef, 1, 0);
 				totalLifeForm += scanCurrentRow(landArray, iRef, jRef, 2, 1);
 				totalLifeForm += scanFinalRow(landArray, iRef, jRef, 1, 0);
